88fa07b5de00cf702bd51415f6ca9ee2d11c6477
wonytest0-dev <wonytest0@gmail.com>
2026-05-15 10:47:40 +0700

    Update fetch-artist.js

--- fetch-artist.js
+++ fetch-artist.js
@@ -1,1 +1,105 @@
+const fs = require("fs");
+
+const TOKEN =
+process.env.GH_TOKEN;
+
+const OWNER =
+"spotify-chart-hit";
+
+const REPO =
+"artist";
+
+const FILE =
+"artist.json";
+
+async function fetchArtist() {
+
+try {
+
+const response =
+await fetch(
+
+`https://api.github.com/repos/${OWNER}/${REPO}/contents/${FILE}`,
+
+{
+headers: {
+Authorization:
+`token ${TOKEN}`,
+
+Accept:
+"application/vnd.github.v3.raw"
+}
+}
+
+);
+
+if (
+
+response.status !== 200
+
+) {
+
+throw new Error(
+`Failed artist 😭 ${response.status}`
+);
+
+}
+
+const data =
+await response.json();
+
+fs.mkdirSync(
+"data/history",
+{
+recursive: true
+}
+);
+
+if (
+
+fs.existsSync(
+"data/artist.json"
+)
+
+) {
+
+fs.copyFileSync(
+
+"data/artist.json",
+
+"data/history/yesterday-artist.json"
+
+);
+
+}
+
+fs.writeFileSync(
+
+"data/artist.json",
+
+JSON.stringify(
+data,
+null,
+2
+)
+
+);
+
+console.log(
+"artist downloaded 😍"
+);
+
+}
+
+catch (err) {
+
+console.log(
+err.message
+);
+
+}
+
+}
+
+fetchArtist();
 
